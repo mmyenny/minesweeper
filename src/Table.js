b82f3f7103408bd27375d7035376141e7af27dc7
Michelle Yenny <mmy491@gmail.com>
2018-11-15 17:00:31 -0500

    Minesweeper

---
+++ src/Table.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react'
+import Cell from './Cell'
+
+class Table extends Component {
+  render() {
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <Cell value="0" />
+            <Cell value="1" />
+            <Cell value="2" />
+            <Cell value="3" />
+            <Cell value="4" />
+            <Cell value="5" />
+            <Cell value="6" />
+            <Cell value="7" />
+          </tr>
+          <tr>
+            <Cell value="0" />
+            <Cell value="1" />
+            <Cell value="2" />
+            <Cell value="3" />
+            <Cell value="4" />
+            <Cell value="5" />
+            <Cell value="6" />
+            <Cell value="7" />
+          </tr>
+          <tr>
+            <Cell value="0" />
+            <Cell value="1" />
+            <Cell value="2" />
+            <Cell value="3" />
+            <Cell value="4" />
+            <Cell value="5" />
+            <Cell value="6" />
+            <Cell value="7" />
+          </tr>
+          <tr>
+            <Cell value="0" />
+            <Cell value="1" />
+            <Cell value="2" />
+            <Cell value="3" />
+            <Cell value="4" />
+            <Cell value="5" />
+            <Cell value="6" />
+            <Cell value="7" />
+          </tr>
+          <tr>
+            <Cell value="0" />
+            <Cell value="1" />
+            <Cell value="2" />
+            <Cell value="3" />
+            <Cell value="4" />
+            <Cell value="5" />
+            <Cell value="6" />
+            <Cell value="7" />
+          </tr>
+          <tr>
+            <Cell value="0" />
+            <Cell value="1" />
+            <Cell value="2" />
+            <Cell value="3" />
+            <Cell value="4" />
+            <Cell value="5" />
+            <Cell value="6" />
+            <Cell value="7" />
+          </tr>
+          <tr>
+            <Cell value="0" />
+            <Cell value="1" />
+            <Cell value="2" />
+            <Cell value="3" />
+            <Cell value="4" />
+            <Cell value="5" />
+            <Cell value="6" />
+            <Cell value="7" />
+          </tr>
+          <tr>
+            <Cell value="0" />
+            <Cell value="1" />
+            <Cell value="2" />
+            <Cell value="3" />
+            <Cell value="4" />
+            <Cell value="5" />
+            <Cell value="6" />
+            <Cell value="7" />
+          </tr>
+        </tbody>
+      </table>
+    )
+  }
+}
+
+export default Table
